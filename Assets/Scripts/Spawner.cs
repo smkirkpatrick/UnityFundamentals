5fe490ce851987ea8abe3106d79e17044be65aca
Sean Kirkpatrick <seanmkirkpatrick@gmail.com>
2016-01-15 16:19:11 -0500

    sphere spawner from within a cube on spacebar

---
+++ Assets/Scripts/Spawner.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Spawner : MonoBehaviour
+{
+	public GameObject spawnType = null;
+
+	void Update()
+	{
+		if (Input.GetKeyDown(KeyCode.Space)) {
+			GameObject obj = Instantiate ( spawnType ) as GameObject;
+			obj.transform.position = this.transform.position;
+			obj.AddComponent<Rigidbody>();
+		}
+	}
+}
