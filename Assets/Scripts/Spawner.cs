7e6b6de90ecf5f07f31235d02a270feb63f3e84e
Sean Kirkpatrick <seanmkirkpatrick@gmail.com>
2016-01-16 15:22:45 -0500

    weird issue observed with sphere being too small for plane mesh collider and falling through - made sphere game object a prefab so that the spawner doesn't need to use an object from the scene

--- Assets/Scripts/Spawner.cs
+++ Assets/Scripts/Spawner.cs
@@ -11,6 +11,7 @@ public class Spawner : MonoBehaviour
 			GameObject obj = Instantiate ( spawnType ) as GameObject;
 			obj.transform.position = this.transform.position;
 			obj.AddComponent<Rigidbody>();
+			obj.name = "mySphere";
 		}
 	}
 }
