bffc962a248b002e64bf5f88035a756d47e36bcf
Sean Kirkpatrick <seanmkirkpatrick@gmail.com>
2016-01-18 14:44:32 -0500

    attached script to main camera that will follow player, created child shape to indicate front of player object

---
+++ Assets/Scripts/Cameras.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Cameras : MonoBehaviour
+{
+	public Transform player = null;
+
+	void LateUpdate ()
+	{ 	// ^ Happens after Update() in the game loop
+		this.transform.LookAt (player.position);
+	}
+}
